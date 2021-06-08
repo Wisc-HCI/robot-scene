1cd09bf325150eb6b6963d541bdad6acd69ac810
Andrew Schoen <schoen.andrewj@gmail.com>
2021-06-07 20:48:02 -0500

    Improved lines

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { Line } from '@react-three/drei';
 import useSceneStore from './SceneStore';
 
 export default function SceneLine(props) {
   const { lineKey } = props;
-  const {name, vertices} = useSceneStore(state => ({name: state.lines[lineKey].name, vertices: state.lines[lineKey].vertices}));
+  const {name, vertices, width} = useSceneStore(useCallback(state => ({
+    name: state.lines[lineKey].name, 
+    vertices: state.lines[lineKey].vertices,
+    width: state.lines[lineKey].width
+  }),[lineKey]));
 
   return (
     <Line
       points={vertices.map(vertex=>([vertex.position.x,vertex.position.y,vertex.position.z]))}
       color='white'
       vertexColors={vertices.map(vertex=>([vertex.color.r/255,vertex.color.g/255,vertex.color.b/255]))}
-      lineWidth={3}
+      lineWidth={width}
     />
   )
 }
