1f526fc3b07017aada3b5b135aa7c61436f680cf
Andrew Schoen <schoen.andrewj@gmail.com>
2021-11-12 13:37:33 -0600

    revised file loading

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
-import url from '@rollup/plugin-url';
+// import url from '@rollup/plugin-url';
+import files from 'rollup-plugin-import-file';
 import pkg from './package.json';
 
 export default {
@@ -17,7 +18,12 @@ export default {
         exclude: 'node_modules/**',
         presets: ['@babel/preset-env','@babel/preset-react']
       }),
-      url({include:'**/*.glb'}),
+    //   url({include:'**/*.glb'}),
+      files({
+        output: 'dist/assets',
+        extensions: /\.(glb)$/,
+        hash: false,
+      }),
       resolve(),
       commonjs(),
       terser()
